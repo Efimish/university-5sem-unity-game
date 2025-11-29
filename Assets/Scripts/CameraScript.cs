64e2d2835ccfcff960899a2d4b4fa6f7c3f0ef22
BombasticFantastik <artemilusenko2@gmail.com>
2025-11-29 23:47:58 +0700

    добавил движение камеры за героем

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 pos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!player)
+        {
+            player=FindObjectOfType<Anonimus>().transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        pos=player.position;
+        Debug.Log(pos);
+        pos.z=-5f;
+        pos.y+=6f;
+        transform.position=Vector3.Lerp(transform.position,pos,0.05f);
+        
+    }
+}
